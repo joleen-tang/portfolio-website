6a8bf6a960373b46ddf70a19b3c0160bee107824
Joleen Tang <jtan0045@student.monash.edu>
2021-02-12 23:46:38 +1100

    Stickied and added toggle to nav bar

--- js/website-scripts.js
+++ js/website-scripts.js
@@ -1,3 +1,16 @@
-function testAlert(){
-	alert("Hello World");
+function addEventListeners(){
+}
+
+function toggleNavDisplay(elementId){
+	var element=document.getElementById(elementId);
+	if (element.style.display=="none"){
+		element.style.width="15%";
+		element.nextElementSibling.style.width="85%";
+		element.style.display="inline-block";
+		return;
+	} else{
+	element.style.width="0%";
+	element.nextElementSibling.style.width="100%";
+	element.style.display="none";
+	}
 }
