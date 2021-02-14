8f06aefe87c8c5ccca517010fb6b9e688720cd02
Joleen Tang <jtan0045@student.monash.edu>
2021-02-14 16:27:12 +1100

    Divided into subsections, started using JQuery

--- js/website-scripts.js
+++ js/website-scripts.js
@@ -3,14 +3,9 @@ function addEventListeners(){
 
 function toggleNavDisplay(elementId){
 	var element=document.getElementById(elementId);
-	if (element.style.display=="none"){
-		element.style.width="15%";
-		element.nextElementSibling.style.width="85%";
-		element.style.display="inline-block";
-		return;
-	} else{
-	element.style.width="0%";
-	element.nextElementSibling.style.width="100%";
-	element.style.display="none";
-	}
+	$("#"+elementId).toggle(350, "linear");
+}
+
+function toggleElementVisibility(ElementId){
+
 }
