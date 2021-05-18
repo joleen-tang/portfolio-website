4a522f76cbd4238584623bc33ee70eb6f4b2d2c8
Joleen Tang <jtan0045@student.monash.edu>
2021-05-18 17:56:27 +1000

    Created Greenhouse database and inserted values for seed table

--- python/seed_setup.py
+++ python/seed_setup.py
@@ -1,5 +1,29 @@
 import sqlite3
+import csv
 
-con = sqlite3.connect('greenhouse.db')
-cur = con.cursor()
+
+def setUpSeedTable():
+    con = sqlite3.connect('greenhouse.db')
+
+    cur = con.cursor()
+    cur.execute('DROP TABLE seed')
+    cur.execute(
+        'CREATE TABLE IF NOT EXISTS seed('
+        'seedID INTEGER PRIMARY KEY,name TEXT, rank INTEGER, rarity INTEGER, stat TEXT, buyable INTEGER)')
+
+    seedsCSV = open('FE3H_Seeds.csv')
+
+    try:
+        csv_reader = csv.reader(seedsCSV, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count > 0:
+                cur.execute(
+                    f'INSERT INTO seed(name, rank, rarity, stat, buyable) '
+                    f'VALUES ("{row[0]}", {row[1]}, {row[2]}, "{row[3]}", {row[4]})')
+            line_count += 1
+    finally:
+        seedsCSV.close()
+
+    cur.close()
 
